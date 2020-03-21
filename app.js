b8461a7c3b0137cb94623ae5dddd6f37102f3b16
Ramigojag <60648146+Ramigojag@users.noreply.github.com>
2020-03-21 18:03:06 +1100

    update main files

--- app.js
+++ app.js
@@ -11,33 +11,28 @@ var mealList = [
 	'spicy brocolli mushroom noodles',
 	'stuffed peppers'
 ];
+//variables
 var e = document.getElementById('days');
 var num = e.options[e.selectedIndex].value;
-var str = '<ul>';
+var str = '';
+var displayList = document.getElementById('mealPlan');
 var someFunction = function() {
 	boxValue = document.getElementById('new').value;
 	mealList.push(boxValue);
 	console.log(mealList);
 	document.getElementById('new').val('');
 };
-
 //click generate to display number of meals selected in drop down box
+//table shows only the number of meals selected doesnt display empty <li>'s
 document.getElementById('gen').addEventListener('click', function() {
 	for (var i = 0; i < e.options[e.selectedIndex].value; i++) {
+		//randomize
 		let randomMeal = mealList[Math.floor(Math.random() * mealList.length)];
-		console.log(randomMeal);
+		//create <li>
+		$('ul').append('<li>' + randomMeal + '</li>');
 	}
-
-	// for (var i = 0; i < e.options[e.selectedIndex].value; i++) {
-	// 	mealList.forEach(function(slide) {
-	// 		str += '<li>' + slide + '</li>';
-	// 	});
-
-	// 	str += '</ul>';
-	// 	document.getElementById('mealPlan').innerHTML = str;
-	// }
+	//
 });
-//table shows only the number of meals selected doesnt display empt LIs
 
 //user inputs new meals, added to mealList array
 $("input[type='text']").keypress(function(event) {
